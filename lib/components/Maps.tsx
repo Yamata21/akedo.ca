f6ea87dfef8bd2f9788e7a1f367c944ffe23eb09
surajgoraya <4347494+surajgoraya@users.noreply.github.com>
2026-09-01 21:03:49 -0400

    fix: update google maps to use correct locale

--- lib/components/Maps.tsx
+++ lib/components/Maps.tsx
@@ -9,18 +9,26 @@ interface MapsProps {
   embedCode?: string;
   src?: string;
   className?: string;
+  themed?: boolean;
 }
 
-export default function Maps({ embedCode, src, className }: MapsProps) {
+export default function Maps({
+  embedCode,
+  src,
+  className,
+  themed = true,
+}: MapsProps) {
   return (
     <div
-      className={`border-brand-purple relative rounded-2xl border-2 ${className}`}
+      className={`${themed ? "border-brand-purple" : ""} relative ${themed ? "rounded-2xl border-2" : "h-full"} ${className}`}
     >
-      <div className="bg-brand-purple pointer-events-none absolute z-10 h-72 w-full opacity-70 mix-blend-hue">
+      <div
+        className={`${themed ? "bg-brand-purple" : ""} pointer-events-none absolute z-10 ${themed ? "h-72" : "h-full"} w-full ${themed ? "opacity-70 mix-blend-hue" : ""}`}
+      >
         &nbsp;
       </div>
       <iframe
-        className="h-72 w-full rounded-[14px]"
+        className={`${themed ? "h-72" : "h-full"} w-full rounded-[14px]`}
         src={src ?? `https://www.google.com/maps/embed?pb=${embedCode}`}
         allowFullScreen={false}
         loading="lazy"
